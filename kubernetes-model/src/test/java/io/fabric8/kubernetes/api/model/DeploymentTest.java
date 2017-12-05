93fa24fe051f84791c67fd3977b5599a232a5718
Piyush Garg <piyushgarg001@gmail.com>
2017-12-05 15:28:50 +0530

    Added licence text and formatted function name

--- kubernetes-model/src/test/java/io/fabric8/kubernetes/api/model/DeploymentTest.java
+++ kubernetes-model/src/test/java/io/fabric8/kubernetes/api/model/DeploymentTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2011 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.fabric8.kubernetes.api.model;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,7 +30,7 @@ public class DeploymentTest {
     private final ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void KubernetesDeploymentTest() throws Exception {
+    public void kubernetesDeploymentTest() throws Exception {
         // given
         final String originalJson = Helper.loadJson("/valid-deployment.json");
 
@@ -29,7 +44,7 @@ public class DeploymentTest {
     }
 
     @Test
-    public void OpenshiftDeploymentConfigTest() throws Exception {
+    public void openshiftDeploymentConfigTest() throws Exception {
         // given
         final String originalJson = Helper.loadJson("/valid-deploymentConfig.json");
 
